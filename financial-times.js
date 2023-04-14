38fd5e1d7a91c19a4450c60e8e0875a71b8b4f98
Dedekind561 <mitchell.lloyd154@gmail.com>
2023-04-14 12:02:12 +0100

    add solutions for financial times

---
+++ financial-times.js
@@ -0,0 +1,127 @@
+function potentialHeadlines(allArticleTitles) {
+  let headlines = [];
+
+  for (let title of allArticleTitles) {
+    if (title.length <= 65) {
+      headlines.push(title);
+    }
+  }
+
+  return headlines;
+}
+
+/*
+    The editor of the FT likes short headlines with only a few words!
+    Implement the function below, which returns the title with the fewest words.
+    (you can assume words will always be seperated by a space)
+*/
+function titleWithFewestWords(allArticleTitles) {
+  let fewestWordsSoFar;
+  let titleWithFewestWords;
+
+  for (let title of allArticleTitles) {
+    // working out the number of words in the title by splitting on the space character
+    // this will generate an array. Read more: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+    let numWords = title.split(" ").length;
+
+    if (fewestWordsSoFar === undefined || numWords < fewestWordsSoFar) {
+      fewestWordsSoFar = numWords;
+      titleWithFewestWords = title;
+    }
+  }
+
+  return titleWithFewestWords;
+}
+
+/*
+    The editor of the FT has realised that headlines which have numbers in them get more clicks!
+    Implement the function below to return a new array containing all the headlines which contain a number.
+    (Hint: remember that you can also loop through the characters of a string if you need to)
+*/
+function headlinesWithNumbers(allArticleTitles) {
+  let articlesWithNumbers = [];
+
+  for (let title of allArticleTitles) {
+    // Making use of the new function created below
+    if (doesTitleContainANumber(title)) {
+      articlesWithNumbers.push(title);
+    }
+  }
+
+  return articlesWithNumbers;
+}
+
+// Creating another function to help break this problem down into smaller parts
+function doesTitleContainANumber(title) {
+  for (let character of title) {
+    if (character >= "0" && character <= "9") {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+/*
+    The Financial Times wants to understand what the average number of characters in an article title is.
+    Implement the function below to return this number - rounded to the nearest integer.
+*/
+function averageNumberOfCharacters(allArticleTitles) {
+  let totalCharacters = 0;
+
+  for (let title of allArticleTitles) {
+    totalCharacters += title.length;
+  }
+
+  return Math.round(totalCharacters / allArticleTitles.length);
+}
+
+/* ======= List of Articles - DO NOT MODIFY ===== */
+const ARTICLE_TITLES = [
+  "Streaming wars drive media groups to spend more than $100bn on new content",
+  "Amazon Prime Video India country head: streaming is driving a TV revolution",
+  "Aerospace chiefs prepare for bumpy ride in recovery of long-haul flights",
+  "British companies look to muscle in on US retail investing boom",
+  "Libor to take firm step towards oblivion on New Year's Day",
+  "Audit profession unattractive to new recruits, says PwC boss",
+  "Chinese social media users blast Elon Musk over near miss in space",
+  "Companies raise over $12tn in 'blockbuster' year for global capital markets",
+  "The three questions that dominate investment",
+  "Brussels urges Chile's incoming president to endorse EU trade deal",
+];
+
+/* ======= TESTS - DO NOT MODIFY ===== */
+
+test("should only return potential headlines", () => {
+  expect(new Set(potentialHeadlines(ARTICLE_TITLES))).toEqual(
+    new Set([
+      "British companies look to muscle in on US retail investing boom",
+      "Libor to take firm step towards oblivion on New Year's Day",
+      "Audit profession unattractive to new recruits, says PwC boss",
+      "The three questions that dominate investment",
+    ])
+  );
+});
+
+test("should return an empty array for empty input", () => {
+  expect(potentialHeadlines([])).toEqual([]);
+});
+
+test("should return the title with the fewest words", () => {
+  expect(titleWithFewestWords(ARTICLE_TITLES)).toEqual(
+    "The three questions that dominate investment"
+  );
+});
+
+test("should only return headlines containing numbers", () => {
+  expect(new Set(headlinesWithNumbers(ARTICLE_TITLES))).toEqual(
+    new Set([
+      "Streaming wars drive media groups to spend more than $100bn on new content",
+      "Companies raise over $12tn in 'blockbuster' year for global capital markets",
+    ])
+  );
+});
+
+test("should return the average number of characters in a headline", () => {
+  expect(averageNumberOfCharacters(ARTICLE_TITLES)).toEqual(65);
+});

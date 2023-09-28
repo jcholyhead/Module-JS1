2896c69f04fd73a077c407e41e3a3a5f41d1f367
jcholyhead <92216197+jcholyhead@users.noreply.github.com>
2023-09-28 23:13:49 +0100

    Updated the instructions to clarify the intent of the exercise
    Given this exercise is about creating initials from a name, I would anticipate that the expected output would be "CKJ" (the person's initials), but the instructions as written would lead me to write a function that outputs "CREKATJOH" - if that is the intent, then I would suggest reworking the example so it isn't initials.

--- week-1/exercises/initials.js
+++ week-1/exercises/initials.js
@@ -2,5 +2,5 @@ let firstName = "Creola";
 let middleName = "Katherine";
 let lastName = "Johnson";
 
-// Declare a variable called initials that stores the first 3 characters of each string in upper case
+// Declare a variable called initials that stores the first characters from each string in upper case to represent the user's initials
 // Log the variable in each case
